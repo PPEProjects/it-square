44471bb8d56b6ad6d32b9afc6916dd75391304e2
Yuan <nguyenshort@gmail.com>
2022-08-15 14:25:03 +0700

    add layout

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,14 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    AButton: typeof import('ant-design-vue/es')['Button']
-    ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
-    AForm: typeof import('ant-design-vue/es')['Form']
-    AFormItem: typeof import('ant-design-vue/es')['FormItem']
-    AInput: typeof import('ant-design-vue/es')['Input']
-    AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
-    ASwitch: typeof import('ant-design-vue/es')['Switch']
-    AuthLayout: typeof import('./../src/components/auth/AuthLayout.vue')['default']
     HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
     MasterLayout: typeof import('./../src/components/includes/MasterLayout.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
