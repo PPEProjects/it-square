e6438d05b062165ec5bd6d3bd3b81f735f7672ad
Yuan <nguyenshort@gmail.com>
2022-09-05 16:05:54 +0700

    add footer

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,6 +8,8 @@
 <!--    <why-us />-->
 <!--    <div class='h-7'></div>-->
     <home-running />
+    <div class='h-7'></div>
+    <home-done />
     <div class='h-3'></div>
     <show-case />
     <div class='h-7'></div>
